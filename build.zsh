3463e714bdd8a770dcd20abab9c146e7cec44cc1
kuss <kuss@dynamic-pd01.res.v6.highway.a1.net>
2026-04-22 19:44:30 +0200

    #

--- build.zsh
+++
@@ -1,9 +0,0 @@
-echo "Setting up cozy coding area"
-#vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-#everforest theme
-curl -L https://github.com/sainnhe/everforest/archive/refs/heads/master.tar.gz | \
-tar -xz -C ~/.vim --strip-components=1 \
-"everforest-master/colors" \
-"everforest-master/autoload"
